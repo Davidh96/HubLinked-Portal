78cc64908dd855885200677ab60786eb27ba5042
caoimheharvey <caoimhe.e.harvey@gmail.com>
2017-03-19 13:29:31 +0800

    01 RESOLVING MERGE ISSUE

--- web/index.php
+++ web/index.php
@@ -1,12 +1,42 @@
 <?php
+<<<<<<< HEAD
 echo "Hello World";
+=======
+echo "hello";
+>>>>>>> a97cf4f9fae14e8b0cd99a7ab69a89d6849f5288
 
-require('../vendor/autoload.php');
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["postgres://wsktsvaretvdmj:234109bb12c27368ebfbb9fc085679ddd2e1e6ed338f2ccd4737957e970bf43f@ec2-54-75-248-193.eu-west-1.compute.amazonaws.com:5432/d4dfkncd7c1kqj"]));
+  return "user=$user password=$pass host=$host sslmode=require dbname=" . substr($path, 1); 
+}
 
+
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables");
+print "<pre>\n";
+
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+
+/*
+include('/vendor/autoload.php');
+echo "after require";
 $app = new Silex\Application();
 $app['debug'] = true;
+<<<<<<< HEAD
 /*
 $dbopts = parse_url(getenv('DATABASE_URL'));
+=======
+$DATABASE_URL = "postgres://wsktsvaretvdmj:234109bb12c27368ebfbb9fc085679ddd2e1e6ed338f2ccd4737957e970bf43f@ec2-54-75-248-193.eu-west-1.compute.amazonaws.com:5432/d4dfkncd7c1kqj";
+$dbopts = parse_url(getenv($DATABASE_URL));
+>>>>>>> a97cf4f9fae14e8b0cd99a7ab69a89d6849f5288
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
@@ -51,5 +81,6 @@ $app->get('/', function() use($app) {
 echo "Hello World";
 
 $app->run();
-
+*/
+?>
 
