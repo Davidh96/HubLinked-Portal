811791915b7a72a90830546b4935c8429778bd10
CharlesAKA <kwebina-ac@hotmail.co.uk>
2017-04-10 15:06:29 +0100

    POPULATED HOME PAGE

--- web/Homepage.php
+++ web/Homepage.php
@@ -5,15 +5,36 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link href="stylesheet.css" rel="stylesheet" type="text/css"/>
+
 </head>
 <body>
   <?php
     require "navigationbar.html"
    ?>
-  <div class = "Jumbotron">
-            <div  class="container">
-              <h2 class="header-text">Homepage</h2>
-              <hr>
-            </div>
-        </div></body>
+   <div id="menu">
+     <div class="jumbotron col-lg-4 col-md-4 col-sm-12 col-xs-12" id="industryPartners">
+       <h2>Industry Partners</h2>
+       <ul>
+         <li>Microsoft</li>
+         <li>Intel</li>
+      </ul>
+     </div>
+     <div class="jumbotron col-lg-4 col-md-4 col-sm-12 col-xs-12" id="Universities">
+       <h2>Universities</h2>
+       <ul>
+         <li>Dublin Institute of Technology</li>
+         <li>University College Dublin</li>
+      </ul>
+     </div>
+     <div class="jumbotron col-lg-4 col-md-4 col-sm-12 col-xs-12" id="Locations">
+       <h2>Locations</h2>
+       <ul>
+         <li>Dublin</li>
+         <li>Seoul</li>
+      </ul>
+     </div>
+   </div>
+</body>
 </html>
