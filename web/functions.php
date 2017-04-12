231144b40bbc99ed1df428d41b5a44e3ad3c08ec
Vardaan <vardaansharma96@gmail.com>
2017-04-12 23:52:03 +0400

    test

--- web/functions.php
+++ web/functions.php
@@ -26,7 +26,7 @@ function pg_check_table($thing, $table, $data){
 
 function pg_check_for_tables(){
     $conn = pg_connect(changeURL());
-    $result = pg_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+    $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 print "<pre>\n";
 
 if (!pg_num_rows($result)) {
