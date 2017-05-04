cc3de20dad37c9b6fc5b0c4aa6b86a8832265504
David Hunt <c14464428@mydit.ie>
2017-05-04 16:25:06 +0800

    Updated homepage with details about opp

--- web/Homepage.php
+++ web/Homepage.php
@@ -36,12 +36,31 @@ else{
 				<a href="user_account.php" class="btn btn-primary" role="button">View Profile</a> <br><br>
 			</div>
 
-
 			<div class="well">
 				<div class="row">
 					<div class="col-sm-12">
-						<h4><small>Summary</small></h4>
-						<p></p>
+						<h3><small>Latest Opportunities</small></h3>
+						<table width="200" class="table table-condensed">
+						<tr>
+										<td> company</td>
+										<td>title</td>
+										<td>location</td>
+										<td> Click here for informaton </td>
+						</tr>
+						<?php
+						$stmt = $myPDO->query("SELECT op_title,comp_name,loc_name from opportunity JOIN company ON author_id = comp_id JOIN location using(loc_id) ORDER BY op_id DESC limit 10");
+
+						while ($row = $stmt->fetch()){
+						echo "<tr>
+								<td> $row[1]</td>
+								<td>$row[0]</td>
+								<td>$row[2]</td>
+								<td> Click here for informaton </td>
+						</tr>";
+								}
+							?>
+
+						</table>
 					</div>
 				</div>
 			</div>
@@ -62,10 +81,31 @@ else{
 					?>
 				<br>
 					<a href="view_applicants.php">View Applicants</a><hr>
+					<?php
+					if($_SESSION["usertype"] != "STUDENT"){
 
-          <h4>Posted Opportunities</h4>
-					Few lists of posted opportunities<br>
-					<a href="view_opptnity.php">Manage</a>
+						echo'<h4>Posted Opportunities</h4>
+						<a href="view_opptnity.php">Manage</a>';
+
+
+						echo '<table width="200" class="table table-condensed">';
+
+
+						$stmt = $myPDO->prepare("SELECT op_title from opportunity JOIN company ON author_id = comp_id where comp_email = :se limit 5");
+						$stmt->bindParam(':se',$email);
+						$stmt->execute();
+
+						while ($row = $stmt->fetch()){
+						echo "<tr>
+								<td> $row[0]</td>
+								<td> Click here for informaton </td>
+						</tr>";
+								}
+						}
+						?>
+
+					</table>
+
 
 				</div>
 			</div>
